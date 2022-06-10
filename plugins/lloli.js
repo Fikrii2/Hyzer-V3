e41eba8973feec9395cd619a3723d7ba9c8c37b3
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-10 19:50:54 +0700

    Update lloli.js

--- plugins/lloli.js
+++ plugins/lloli.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
      let handler  = async (m, { conn, usedPrefix, command }) => {
      	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
-    heum = await fetch(`https://api.lolhuman.xyz/api/random/nsfw/loli?apikey=rey2k21`)
+    heum = await fetch(`https://api.lolhuman.xyz/api/random/nsfw/loli?apikey=Fiktod`)
     json = await heum.buffer()
    conn.sendButtonImg(m.chat, json, 'Lomlinya kak\nterkadang gambar mengandung nsfw', watermark, 'NEXT', `${usedPrefix + command}`, m, false)
 }
