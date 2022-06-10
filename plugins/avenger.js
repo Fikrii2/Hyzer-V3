2e7770bb18074a806a7b1bcacc505e4a5b84db23
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-11 01:20:40 +0700

    Update avenger.js

--- plugins/avenger.js
+++ plugins/avenger.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
   if (!args[0]) throw 'Masukkan Parameter'
   m.reply('```Dalam Proses...```')
-  let res = await fetch(`http://api.lolhuman.xyz/api/textprome2/avenger?apikey=rey2k21&text1=${response[0]}&text2=${response[1]}`)
+  let res = await fetch(`https://api.lolhuman.xyz/api/textprome2/avenger?apikey=Fiktod&text1=${response[0]}&text2=${response[1]}`)
   conn.sendFile(m.chat, res, 'nama.jpg', `Nih Mhank`, m, false)
 }
 handler.help = ['avengers'].map(v => v + ' <teks|teks>')
