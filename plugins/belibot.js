e6f08ac568cb84d5e618e07492a9740fc31147d8
Fikrii2 <107239192+Fikrii2@users.noreply.github.com>
2022-06-11 00:58:18 +0700

    Update belibot.js

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
-	conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╠═〘 Beli Bot 〙 ═
+	conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╠═〘 Sewa Bot 〙 ═
 ╠➥ *1 Minggu* : *Rp 5.000*
 ╠➥ *1 Bulan* :  *Rp 10.000*
 ╠➥ *Permanen* : *Rp 25.000*
-║
+╠➥ *Premium 1 Bulan* : *Rp 10.000*
 ╠═〘 𝐏𝐄𝐌𝐁𝐀𝐘𝐀𝐑𝐀𝐍 〙 ═
 ╠➥ Dana/Gopay
 ║
